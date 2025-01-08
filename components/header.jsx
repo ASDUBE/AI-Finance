6f9370bf4b7d3e2c751e39329e7970d199a16118
ASDUBE <ashutoshd6198@gmail.com>
2025-01-08 14:54:22 +0530

    db configured

--- components/header.jsx
+++ components/header.jsx
@@ -4,10 +4,12 @@ import Link from 'next/link';
 import React from 'react';
 import { Button } from './ui/button';
 import { LayoutDashboard, PenBox } from 'lucide-react';
+import { checkUser } from '@/lib/check_user';
 
 // We may have problem we can go to 6.9.0
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
