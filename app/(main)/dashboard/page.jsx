0e3def72fc18c29be5b1f999eac3239211695c5d
ASDUBE <ashutoshd6198@gmail.com>
2025-01-10 23:52:23 +0530

    layout

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="">
+      {/* Budget Progress */}
+
+      {/* Dashboard Overview */}
+
+      {/* Accounts */}
+    </div>
+  );
+};
+
+export default Dashboard;
