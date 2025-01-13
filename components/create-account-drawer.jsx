69ea9367aa8163223690ee5e173d55ee5d0ac105
ASDUBE <ashutoshd6198@gmail.com>
2025-01-13 23:57:52 +0530

    Drawer completed

--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -21,6 +21,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from './ui/select';
+import { Switch } from './ui/switch';
 
 const CreateAccountDrawer = ({ children }) => {
   const [open, setOpen] = useState(false);
@@ -41,6 +42,8 @@ const CreateAccountDrawer = ({ children }) => {
       isDefault: false,
     },
   });
+
+  const onSubmit = async (data) => {};
   return (
     <Drawer open={open} onOpenChange={setOpen}>
       <DrawerTrigger asChild>{children}</DrawerTrigger>
@@ -49,7 +52,7 @@ const CreateAccountDrawer = ({ children }) => {
           <DrawerTitle>Create New Account</DrawerTitle>
         </DrawerHeader>
         <div className="px-4 pb-4">
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
             <div className="space-y-2">
               <label htmlFor="name" className="text-sm font-medium">
                 Account Name
@@ -82,6 +85,51 @@ const CreateAccountDrawer = ({ children }) => {
               {errors.type && (
                 <p className="text-sm text-red-500">{errors.type.message}</p>
               )}
+            </div>
+            <div className="space-y-2">
+              <label htmlFor="balance" className="text-sm font-medium">
+                Initial balance
+              </label>
+              <Input
+                id="balance"
+                type="number"
+                step="0.01"
+                placeHolder="0.00 INR"
+                {...register('balance')}
+              />
+              {errors.balance && (
+                <p className="text-sm text-red-500">{errors.balance.message}</p>
+              )}
+            </div>
+            <div className="flex items-center justify-between rounded-lg border p-3">
+              <div className="space-y-0.5">
+                <label
+                  htmlFor="isDefault"
+                  className="text-sm font-medium cursor-pointer"
+                >
+                  Set as Default Account
+                </label>
+                <p className="text-sm  text-muted-foreground">
+                  This account will be selected by default for transactions
+                </p>
+              </div>
+              <Switch
+                id="isDefault"
+                onCheckedChange={(checked) => setValue('isDefault', checked)}
+                checked={watch('isDefault')}
+              />
+            </div>
+
+            <div>
+              <DrawerClose asChild>
+                <Button type="button" variant="outline" className="flex-1">
+                  Cancel
+                </Button>
+              </DrawerClose>
+
+              <Button type="submit" className="flex-1">
+                Create a account
+              </Button>
             </div>
           </form>
         </div>
